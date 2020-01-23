613be3fabd43cf1ef1c671eb36453a9e077e5eb9
C. Piovesan <clement.piovesan@me.com>
2020-01-23 16:17:16 +0100

    Add match history gathering
    Signed-off-by: C. Piovesan <clement.piovesan@me.com>

---
+++ matches/MatchAnalyser.go
@@ -0,0 +1,163 @@
+package matches
+
+import (
+    "encoding/json"
+    "github.com/hiimkina/riotapi-scrapper/utils"
+)
+
+type ChampionBan struct {
+    PickTurn int
+    ChampionId int
+}
+
+type MatchDetails struct {
+    GameId int
+    ParticipantIdentities []ParticipantDetails
+    GameVersion string
+    Teams []TeamDetails
+    Participants []PlayerStats
+    GameDuration int
+    GameCreation int
+}
+
+type ParticipantDetails struct {
+    Player PlayerDetails
+    ParticipantId int
+}
+
+type PlayerAdvancedStats struct {
+    Assists int
+    ChampLevel int
+    CombatPlayerScore int
+    DamageDoneToTurrets int
+    DamageDealtToObjectives int
+    DamageSelfMitigated int
+    Deaths int
+    DoubleKills int
+    FirstBloodAssist bool
+    FirstBloodKill bool
+    FirstTowerAssist bool
+    FirstTowerKill bool
+    GoldEarned bool
+    GoldSpent bool
+    InhibitorKills int
+    Item0 int
+    Item1 int
+    Item2 int
+    Item3 int
+    Item4 int
+    Item5 int
+    Item6 int
+    KillingSprees int
+    Kills int
+    LargestCriticalStrike int
+    LargestKillingSpree int
+    LargestMultiKill int
+    LongestTimeSpentLiving int
+    MagicDamageDealt int
+    MagicDamageDealtToChampions int
+    MagicDamageTaken int
+    NeutralMinionsKilled int
+    NeutralMinionsKilledTeamJungle int
+    ObjectivePlayerScore int
+    ParticipantId int
+    PentaKills int
+    Perk0 int
+    Perk0Var1 int
+    Perk0Var2 int
+    Perk0Var3 int
+    Perk1 int
+    Perk1Var1 int
+    Perk1Var2 int
+    Perk1Var3 int
+    Perk2 int
+    Perk2Var1 int
+    Perk2Var2 int
+    Perk2Var3 int
+    Perk3 int
+    Perk3Var1 int
+    Perk3Var2 int
+    Perk3Var3 int
+    Perk4 int
+    Perk4Var1 int
+    Perk4Var2 int
+    Perk4Var3 int
+    Perk5 int
+    Perk5Var1 int
+    Perk5Var2 int
+    Perk5Var3 int
+    PerkPrimaryStyle int
+    PerkSubStyle int
+    PhysicalDamageDealt int
+    PhysicalDamageDealtToChampions int
+    PhysicalDamageTaken int
+    QuadraKills int
+    SightWardsBoughtInGame int
+    StatPerk0 int
+    StatPerk1 int
+    StatPerk2 int
+    TimeCCingOthers int
+    TotalDamageDealt int
+    TotalDamageDealtToChampions int
+    TotalDamageTaken int
+    TotalHeal int
+    TotalMinionKilled int
+    TotalPlayerScore int
+    TotalScoreRank int
+    TotalTimeCrowdControlDealt int
+    TotalUnitsHealed int
+    TripleKills int
+    TrueDamageDealt int
+    TrueDamageDealtToChampions int
+    TrueDamageTaken int
+    TurretKills int
+    UnrealKills int
+    VisionScore int
+    VisionWardsBoughtInGame int
+    WardsPlaced int
+    WardsKilled int
+    Win bool
+}
+
+type PlayerDetails struct {
+    CurrentPlatformId string
+    SummonerName string
+    CurrentAccountId string
+    SummonerId string
+}
+
+type PlayerStats struct {
+    Spell1Id int
+    ParticipantId int
+    Spell2Id int
+    TeamId int
+    Stats PlayerAdvancedStats
+    ChampionId int
+}
+
+type TeamDetails struct {
+    FirstDragon bool
+    Bans []ChampionBan
+    FirstInhibitor bool
+    Win bool
+    FirstRiftHerald bool
+    FirstBaron bool
+    BaronKills int
+    RiftHeraldKills int
+    FirstBlood bool
+    TeamId int
+    FirstTower bool
+    InhibitorKills int
+    TowerKills int
+    DragonKills int
+}
+
+func GetMatch ( matchId int ) {
+    apiEndpoint :=  "https://euw1.api.riotgames.com/lol/match/v4/matches/" + matchId
+
+    stringOutput := utils.DoHttpRequest(apiEndpoint)
+    history := new(PlayerHistory)
+    json.Unmarshal([]byte(stringOutput), &history)
+
+    return history
+}
--- riotapi-scrapper.go
+++ riotapi-scrapper.go
@@ -2,26 +2,21 @@ package main
 
 import (
     "fmt"
+    "github.com/hiimkina/riotapi-scrapper/matches"
     "github.com/hiimkina/riotapi-scrapper/summoners"
 )
 
 func main () {
 
     var summonersGathered []summoners.SummonerToAnalyse
-    summonersGathered = summoners.GatherSummoners("RANKED_SOLO_5x5","DIAMOND","I")
+    summonersGathered = summoners.GatherSummoners("RANKED_SOLO_5x5","DIAMOND","I", 1)
 
-    fmt.Printf("%+v\n\n", summonersGathered[0])
-
-    for i := 0; i < 10; i++ {
+    for i := 0; i < 1; i++ {
         summonerToScrap := new(summoners.Summoner)
-        fmt.Printf("\nScrapping: %s\n", summonersGathered[i].SummonerId)
         summonerToScrap = summoners.GetSummonerFromId(summonersGathered[i].SummonerId)
-        fmt.Printf("%+v\n\n", summonerToScrap)
+
+        history := new(matches.PlayerHistory)
+        history = matches.FillPlayerHistory(summonerToScrap.AccountId)
     }
 
-    //summonerToScrap := new(summoners.Summoner)
-    //summonerToScrap = summoners.GetSummonerInfo("GV kina")
-
-    //fmt.Printf("%+v\n", summonerToScrap)
-    //fmt.Printf("%+v\n", summonerToScrap.Ranks)
 }
