1670bba24a2381b825c08bbb5a5dec0f5d98dadb
C. Piovesan <clement.piovesan@me.com>
2020-01-23 11:41:01 +0100

    Add generic function for HTTP requests and add Summoner ranked information getter
    Signed-off-by: C. Piovesan <clement.piovesan@me.com>

--- riotapi-scrapper.go
+++ riotapi-scrapper.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+    "fmt"
     "github.com/hiimkina/riotapi-scrapper/summoners"
 )
 
 func main () {
-    summoners.GetSummonerInfo("FrFkina")
+    var summonerToScrap = new(summoners.Summoner)
+    summonerToScrap = summoners.GetSummonerInfo("GV kina")
+
+    fmt.Printf("%+v\n", summonerToScrap)
+    fmt.Printf("%+v\n", summonerToScrap.Ranks)
 }
--- summoners/SummonerGetter.go
+++ summoners/SummonerGetter.go
@@ -1,47 +1,78 @@
 package summoners
 
 import (
-    "fmt"
-    "net/http"
     "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
 )
 
+type RankedInfo struct {
+    Wins int
+    Losses int
+    Rank string
+    Tier string
+    LeaguePoints int
+}
+
 type Summoner struct {
-    Id string
-    AccountId string
-    Puuid string
-    Name string
-    ProfileIconId int
-    RevisionDate int
-    SummonerLevel int
+    Id string           `json:"id"`
+    AccountId string    `json:"accountId"`
+    Puuid string        `json:"puuid"`
+    Name string         `json:"name"`
+    ProfileIconId int   `json:"profileIconId"`
+    RevisionDate int    `json:"revisionDate"`
+    SummonerLevel int   `json:"summonerLevel"`
+    Ranks []RankedInfo
 }
 
-func ConvertSummoner ( toConvert *http.Response, target interface{} ) error {
-    return json.NewDecoder(toConvert.Body).Decode(target)
+func GetSummonerInfo ( summonerName string ) *Summoner {
+    apiEndpoint :=  "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName
+
+    stringOutput := doHttpRequest(apiEndpoint)
+    summoner := new(Summoner)
+    json.Unmarshal([]byte(stringOutput), &summoner)
+    summoner = getSummonerRankedInfo(summoner)
+
+    return summoner
 }
 
-func GetSummonerInfo ( summonerName string ) {
-    var apiEndpoint string
 
-    apiEndpoint =  "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName
 
+func getSummonerRankedInfo ( summoner *Summoner ) *Summoner {
+    apiEndpoint := "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + summoner.Id
+
+    stringOutput := doHttpRequest(apiEndpoint)
+    var rankedQueues []RankedInfo
+    json.Unmarshal([]byte(stringOutput), &rankedQueues)
+    summoner.Ranks = rankedQueues
+
+    return summoner
+}
+
+func doHttpRequest ( apiEndpoint string ) string {
+    apiToken := "RGAPI-80136d22-3d28-43ff-a80c-fee11ce34569"
     req, err := http.NewRequest("GET", apiEndpoint, nil)
     if err != nil {
-       	fmt.Printf("Error generating HTTP request")
+        fmt.Printf("Error generating HTTP request")
     }
-    req.Header.Set("X-Riot-Token", "RGAPI-80136d22-3d28-43ff-a80c-fee11ce34569")
+    req.Header.Set("X-Riot-Token", apiToken)
 
-    resp, err := http.DefaultClient.Do(req)
+    response, err := http.DefaultClient.Do(req)
+    defer response.Body.Close()
     if err != nil {
         fmt.Printf("Error doing the HTTP request")
     }
 
-    defer resp.Body.Close()
-
-    if resp.StatusCode == http.StatusOK {
-        summoner := new(Summoner)
-        ConvertSummoner(resp, summoner)
-
-        fmt.Printf("%+v", summoner)
+    if response.StatusCode != http.StatusOK {
+        log.Fatal("Error in your request: %s", response.StatusCode)
     }
+
+    bodyBytes, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return string(bodyBytes)
 }
